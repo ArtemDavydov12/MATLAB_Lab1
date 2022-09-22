e500af7194ed3ec0c4cb7382a7fbc57e46c58742
Artem Davydov <90029083+ArtemDavydov12@users.noreply.github.com>
2022-09-22 11:39:28 +0300

    Add files via upload

---
+++ Lab_1/lab1_ASP_DavydovA.m
@@ -0,0 +1,62 @@
+%% Creating pitches;
+pitchDur = 1;
+fS = 8000;
+timeArray = 0:1/fS:pitchDur;
+pitchB = sin(2*pi*freqPitch(1)*timeArray);
+pitchA = sin(2*pi*freqPitch(2)*timeArray);
+pitchG = sin(2*pi*freqPitch(3)*timeArray);
+pitchF = sin(2*pi*freqPitch(4)*timeArray);
+pitchE = sin(2*pi*freqPitch(5)*timeArray);
+pitchD = sin(2*pi*freqPitch(6)*timeArray);
+pitchC = sin(2*pi*freqPitch(7)*timeArray);
+
+%% Read the file with pitches;
+fileID = fopen('song.txt', 'r');
+song = fscanf(fileID,'%s');
+whos("song");
+fclose(fileID);
+
+%% Create the song and the file,wav;
+singSong = [pitchF pitchC pitchF pitchC pitchF pitchE pitchE pitchE pitchC pitchE pitchC pitchE pitchF pitchF pitchF pitchC pitchF pitchC pitchF pitchE pitchE pitchE pitchC pitchE pitchC pitchF];
+sound(singSong);
+audiowrite("firstSong.wav", singSong, fS);
+audioWavInfo = audioinfo("firstSong.wav");
+wavFileName = audioWavInfo.Filename;
+wavFileCompMet = audioWavInfo.CompressionMethod;
+wavFileNumCh = audioWavInfo.NumChannels;
+wavFileRate = audioWavInfo.SampleRate;
+wavFileSamples = audioWavInfo.TotalSamples;
+wavFileDur = audioWavInfo.Duration;
+
+%% Listen the mp3 file;
+[y,fs] = audioread('song.mp3');
+sound(y,fs);
+audioMp3Info = audioinfo("song.mp3");
+mp3FileName = audioMp3Info.Filename;
+mp3FileCompMet = audioMp3Info.CompressionMethod;
+mp3FileNumCh = audioMp3Info.NumChannels;
+mp3FileRate = audioMp3Info.SampleRate;
+mp3FileSamples = audioMp3Info.TotalSamples;
+mp3FileDur = audioMp3Info.Duration;
+save ('data.mat', 'mp3FileDur',"mp3FileSamples","mp3FileRate","mp3FileNumCh","mp3FileCompMet","mp3FileName");
+
+%% fPrintf Command Window;
+fprintf('---------- .MP3 VS .WAV ----------\n');
+
+fprintf(blanks(5));
+[filepath, name, ext] = fileparts(wavFileName);
+[filepath1, name1, ext1] = fileparts(mp3FileName);
+fprintf('%s', name,ext, ' VS ',  name1,ext1, newline);
+
+fprintf('%s', blanks(8), wavFileCompMet, ' VS ',  mp3FileCompMet, blanks(1), newline);
+
+fprintf('%s', blanks(14), num2str( wavFileDur, '% .2f'), ' VS ', num2str(mp3FileDur, '% 1.2f'), newline);
+
+fprintf('%s', blanks(5), num2str(wavFileSamples, '% .2f'), ' VS ', num2str(mp3FileSamples, '% .2f'), newline)
+
+fprintf('-----------------------------------------\n');
+%% disp;
+str =[blanks(2), name, ext, ' VS ', name1, ext1, newline, blanks(5), wavFileCompMet, ' VS ', mp3FileCompMet, newline, blanks(7), num2str(wavFileDur), ' VS ', num2str(mp3FileDur), newline, blanks(5), num2str(wavFileSamples, '% .2f'), ' VS ', num2str(mp3FileSamples, '% .2f'), newline];
+disp('---------- .MP3 VS .WAV ----------');
+disp(str);
+disp('-----------------------------------------');
